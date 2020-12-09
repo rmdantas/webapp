c4c615b82335d8995a07594c5d37e372244dc146
Rodrigo Machado Dantas <39016077+rmdantas@users.noreply.github.com>
2020-12-09 17:42:54 -0300

    Fix #2

--- src/main/java/box/dao/ContatoDao.java
+++ src/main/java/box/dao/ContatoDao.java
@@ -7,26 +7,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import box.jdbc.ConnectionFactory;
 import box.modelo.Contato;
 
 public class ContatoDao {
-
+	
+	private static final Logger log = Logger.getLogger("ContatoDao");
 	private Connection connection;
 
 	// Criando a conexão com o banco de dados
-	public ContatoDao() {
-		connection = new ConnectionFactory().getConnection();
+	public ContatoDao() throws SQLException {
+		connection = ConnectionFactory.getConnection();
+		connection.setAutoCommit(false);
 	}
 
 	// Adiciona um contato no banco de dados
 	public void adicionaContato(Contato contato) {
 
-		String sql = "insert into contatos(nome, email, endereco, dataNascimento)" + "values(?,?,?,?);";
+		final String sql = "insert into contatos(nome, email, endereco, dataNascimento) values(?,?,?,?);";
 
-		try {
-			PreparedStatement insert = connection.prepareStatement(sql);
+		try (PreparedStatement insert = connection.prepareStatement(sql)) {
 
 			insert.setString(1, contato.getNome());
 			insert.setString(2, contato.getEmail());
@@ -35,43 +37,39 @@ public class ContatoDao {
 
 			// insert.execute();
 			insert.executeUpdate();
-			insert.close();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new RuntimeException("Erro ao inserir um conteudo no banco de dados");
+			log.warning("Erro ao inserir um conteudo no banco de dados");
 		}
 	}
 
 	// Listando os contatos
 	public List<Contato> getLista() {
-		try {
-			List<Contato> contatos = new ArrayList<>();
-			String sql = "select * from contatos";
+		List<Contato> contatos = new ArrayList<>();
+		final String sql = "select * from contatos";
+		
+		try (PreparedStatement select = connection.prepareStatement(sql)) {
+			try (ResultSet rs = select.executeQuery()) {
+				while (rs.next()) {
 
-			PreparedStatement select = connection.prepareStatement(sql);
-			ResultSet rs = select.executeQuery();
+					Contato contato = new Contato();
 
-			while (rs.next()) {
+					contato.setIdContato(rs.getLong("id_usuario"));
+					contato.setNome(rs.getString("nome"));
+					contato.setEmail(rs.getString("email"));
+					contato.setEndereco(rs.getString("endereco"));
+					contato.setDataNascimento(rs.getDate("dataNascimento"));
 
-				Contato contato = new Contato();
-
-				contato.setIdContato(rs.getLong("id_usuario"));
-				contato.setNome(rs.getString("nome"));
-				contato.setEmail(rs.getString("email"));
-				contato.setEndereco(rs.getString("endereco"));
-				contato.setDataNascimento(rs.getDate("dataNascimento"));
-
-				contatos.add(contato);
+					contatos.add(contato);
+				}
 			}
-
-			rs.close();
-			select.close();
-			return contatos;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new RuntimeException("Erro ao criar uma lista de usuários");
+			return new ArrayList<>();
 		}
+
+		return contatos;
 	}
 
 }
