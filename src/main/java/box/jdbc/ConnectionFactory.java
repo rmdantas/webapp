1a84a57890276de1475c255066e2d00b8fa37257
Rodrigo Machado Dantas <39016077+rmdantas@users.noreply.github.com>
2020-12-09 17:43:48 -0300

    Fix #2

--- src/main/java/box/jdbc/ConnectionFactory.java
+++ src/main/java/box/jdbc/ConnectionFactory.java
@@ -3,6 +3,8 @@ package box.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+
 
 /**
  * Classe responsável por fazer a conexão com o banco de dados
@@ -11,14 +13,27 @@ import java.sql.SQLException;
  */
 public class ConnectionFactory {
 
-	public Connection getConnection() {
-		try {
-			Class.forName("org.postgresql.Driver");
-			System.out.println("Conectado no banco com louvor!");
-			return DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "123");
-		} catch (SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Erro ao se conectar com o banco de dados");
-		}
+	private static final Logger log = Logger.getLogger("ConnectionFactory");
+	
+	private ConnectionFactory() {
+		throw new IllegalStateException("Utility class");
+	}
+	
+	private static Connection create(final String url, final String user, final String password) throws SQLException {
+		Connection connection = DriverManager.getConnection(url, user, password);
+		log.info("Conectado no banco com louvor!");
+		return connection;
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		final String url = "jdbc:postgresql://localhost/postgres";
+		final String user = "postgres";
+		final String password = "123";
+		
+		return create(url, user, password);
+	}
+	
+	public static Connection getConnection(final String url, final String user, final String password) throws SQLException {
+		return create(url, user, password);
 	}
 }
